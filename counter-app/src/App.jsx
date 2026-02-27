0976f0bca275a14cd9c53db67e55f7ac6a53a3be
sameer-das1 <kumarsameer0555@gmail.com>
2026-02-27 20:16:03 +0530

    Initial commit

---
+++ counter-app/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [count, setCount] = useState(0)
+
+
+  return (
+    <>
+      <h1>Sameer das</h1>
+      <h2>Simple Counter value: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </>
+  )
+}
+
+export default App
